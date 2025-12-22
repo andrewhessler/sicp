08bc6d93203b6bc84d82b330e8c4b9828b21ccec
Andrew Hessler <adhessler@gmail.com>
2025-12-21 19:36:31 -0600

    init 1-22

---
+++ exercise1-22.scm
@@ -0,0 +1,16 @@
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime))
+)
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))
+  )
+)
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elpased-time)
+)

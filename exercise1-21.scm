cc5b2c7771dfbdbf08a576355c4ffa29adb03a09
Andrew Hessler <adhessler@gmail.com>
2025-12-21 19:34:24 -0600

    okay, that's that

---
+++ exercise1-21.scm
@@ -0,0 +1,18 @@
+(define (smallest-divisor n)
+  (define (find-divisor n test-divisor)
+    (cond 
+      ((> (square test-divisor) n) n)
+      ((divides? n test-divisor) test-divisor)
+      (else (find-divisor n (+ test-divisor 1)))
+    )
+  )
+  (define (divides? dividend divisor)
+    (= (remainder dividend divisor) 0)
+  )
+  (find-divisor n 2)
+)
+
+(smallest-divisor 20)
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)

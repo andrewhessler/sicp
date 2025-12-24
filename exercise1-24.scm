39b3acb458d95d179a3ee61c3b596a0143387ca1
Andrew Hessler <adhessler@gmail.com>
2025-12-24 11:59:22 -0600

    1-24 so fast

--- exercise1-24.scm
+++ exercise1-24.scm
@@ -29,4 +29,41 @@
   )
 )
 
-(fast-prime? 10000000000000060 10)
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime))
+)
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 10)
+    (report-prime (- (runtime) start-time))
+    #f
+  )
+)
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  #t
+)
+
+(define (search-for-primes start find-count)
+  (cond 
+    ((= find-count 0) 0)
+    ((= (remainder start 2) 0) (search-for-primes (+ start 1) find-count))
+    (else 
+      (if (timed-prime-test start) 
+        (search-for-primes (+ start 2) (- find-count 1))
+        (search-for-primes (+ start 2) find-count)
+      )
+    )
+  )
+)
+
+; 10000000000000061 *** 42.919999999999995 -> 26.93
+; 10000000000000069 *** 42.93000000000001 -> 26.92
+; 10000000000000079 *** 43.019999999999996 -> 27.09
+(search-for-primes 400 2000)
+
+; everything is unmeasurable now... jeez louise

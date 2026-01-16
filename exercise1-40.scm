5b7a53a99634edbe89388d449182f7b7d1c976ee
Andrew Hessler <adhessler@gmail.com>
2026-01-15 18:49:07 -0600

    1.40 good

---
+++ exercise1-40.scm
@@ -0,0 +1,48 @@
+(define (cubic a b c)
+  (lambda (x)
+    (+ 
+      (cube x)
+      (* a (square x))
+      (* b x)
+      c
+    )
+  )
+)
+
+(define dx 0.0000001)
+(define tolerance 0.0000001)
+
+(define (cube x) (* x x x))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x)) dx)
+  )
+)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))
+  )
+)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance)
+  )
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next)
+      ) 
+    )
+  )
+  (try first-guess)
+)
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess)
+)
+
+(newtons-method (cubic 1 2 30) 1) ; -3.2402
